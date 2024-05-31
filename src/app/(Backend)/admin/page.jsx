fc5552e471c263103e81a25cf77d6b0b17677c5d
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-05-31 10:23:11 +0530

    Added signin

---
+++ src/app/(Backend)/admin/page.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { ArrowRight } from 'lucide-react'
+
+export default function AdminSign() {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+          <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in</h2>
+            <p className="mt-2 text-sm text-gray-600">
+              Don&apos;t have an account?{' '}
+              <a
+                href="#"
+                title=""
+                className="font-semibold text-black transition-all duration-200 hover:underline"
+              >
+                Request Access
+              </a>
+            </p>
+            <form action="#" method="POST" className="mt-8">
+              <div className="space-y-5">
+                <div>
+                <label htmlFor="role" className="block text-sm font-medium text-gray-700">
+          Role
+        </label>
+        <select
+          id="role"
+          name="role"
+          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        >
+          <option value="" disabled>Select Role</option>
+          <option value="Admin">Admin</option>
+          <option value="Frontend Desk">Frontend Desk</option>
+          <option value="Teachers">Teachers</option>
+          <option value="Teachers">Data Entry Operator</option>
+          <option value="Teachers">Management</option>
+        </select>
+                </div>
+                <div>
+                  <label htmlFor="" className="text-base font-medium text-gray-900">
+                    {' '}
+                    Email address{' '}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="email"
+                      placeholder="Email"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="" className="text-base font-medium text-gray-900">
+                      {' '}
+                      Password{' '}
+                    </label>
+                    <a
+                      href="#"
+                      title=""
+                      className="text-sm font-semibold text-black hover:underline"
+                    >
+                      {' '}
+                      Forgot password?{' '}
+                    </a>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="password"
+                      placeholder="Password"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                  >
+                    Sign <ArrowRight className="ml-2" size={16} />
+                  </button>
+                </div>
+              </div>
+            </form>
+            
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <img
+            className="mx-auto h-full w-full rounded-md object-cover"
+            src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
